bb795d47bc4d41a75a7908bc60d72c07d11f6010
danielamram <danielamram0@gmail.com>
2023-05-29 22:25:20 +0300

    chore(supabase): add supabase

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
+import { SupabaseAdapter } from "@next-auth/supabase-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+let accessToken = "";
 
 const handler = NextAuth({
   callbacks: {
@@ -9,17 +12,22 @@ const handler = NextAuth({
       }
       return token;
     },
-    async session({ session, token, user }) {
+    async session({ session, token }) {
       // Send properties to the client, like an access_token from a provider.
-      (session as any).accessToken = token.accessToken;
+      session.accessToken = token.accessToken as string;
       return session;
     },
   },
+  session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
 });

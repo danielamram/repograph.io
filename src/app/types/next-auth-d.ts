c41504a311f9da63ae537e8ade1278ad2dff4af1
danielamram <danielamram0@gmail.com>
2023-05-30 00:00:32 +0300

    fix(build): add types

--- src/app/types/next-auth-d.ts
+++ src/app/types/next-auth-d.ts
@@ -4,7 +4,8 @@ declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
+
   interface Session extends DefaultSession {
-    accessToken: string 
+    accessToken: string;
   }
 }
